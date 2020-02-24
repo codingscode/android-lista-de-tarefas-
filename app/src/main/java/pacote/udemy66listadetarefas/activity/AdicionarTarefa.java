c8c657d89fbd314d940796438fb0d5dcb91dc716
Heronnes Pereira <heronnestg@hotmail.com>
2020-02-24 20:32:58 -0300

    parte 5

--- app/src/main/java/pacote/udemy66listadetarefas/activity/AdicionarTarefa.java
+++ app/src/main/java/pacote/udemy66listadetarefas/activity/AdicionarTarefa.java
@@ -2,6 +2,9 @@ package pacote.udemy66listadetarefas.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import pacote.udemy66listadetarefas.R;
 
@@ -11,5 +14,21 @@ public class AdicionarTarefa extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adicionar_tarefa);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_adicionar_tarefa, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.itemSalvar:
+                Toast.makeText(AdicionarTarefa.this, "Botão salvar pressionado", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
